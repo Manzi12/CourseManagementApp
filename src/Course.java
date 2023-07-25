c739bd9cb2f59a2d9ec1950b09977fb8f98ac47f
manzi12 <manzijosephis@gmail.com>
2023-07-25 21:16:50 +0100

    course class created with its getter and setters

---
+++ src/Course.java
@@ -0,0 +1,35 @@
+public class Course {
+    private String courseName;
+    private String instructor;
+    private int credits;
+
+    public Course(String courseName, String instructor, int credits){
+        this.courseName = courseName;
+        this.instructor = instructor;
+        this.credits = credits;
+    }
+
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
+
+    public void setInstructor(String instructor){
+        this.instructor = instructor;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public String getInstructor(){
+        return instructor;
+    }
+
+    public int getCredits(){
+        return credits;
+    }
+}
